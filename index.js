21561941c7b58364febc5240692eccaab982e39a
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-22 15:59:41 -0300

    Adicionado Node.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const hbs = require('express-handlebars')
+const PORT = process.env.PORT || 3000;
+
+//Configuração do HandleBars
+app.engine('hbs', hbs.engine({
+    extname:'hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+
+//rota inicial 
+//rederiza o home.hbs para abra dentro da tag {{{body}}} no layout
+app.get("/", (req, res)=>{
+    res.render('home');
+})
+
+//ativar sistema
+app.listen(PORT, ()=>{
+    console.log('Servidor rodando no http://localhost:'+PORT);
+})
