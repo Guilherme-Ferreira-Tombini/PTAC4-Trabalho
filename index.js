06c1bad7799e923c10f18c4527f20e388ddc0dc1
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-28 22:35:28 -0300

    Adicionado rotas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const hbs = require('express-handlebars')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 //Configuração do HandleBars
 app.engine('hbs', hbs.engine({
@@ -14,6 +14,14 @@ app.set('view engine', 'hbs');
 //rederiza o home.hbs para abra dentro da tag {{{body}}} no layout
 app.get("/", (req, res)=>{
     res.render('home');
+})
+
+app.get("/formulario", (req, res)=>{
+    res.render('formulario');
+})
+
+app.get("/sobre", (req, res)=>{
+    res.render('sobre');
 })
 
 //ativar sistema
