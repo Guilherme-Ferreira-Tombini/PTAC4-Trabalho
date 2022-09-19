50ffa1b0e095bbad43d421c90707efb4f26f21d6
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-09-19 14:36:36 -0400

    Adicionado banco em mysql

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('xmcpwkyj','xmcpwkyj','zctuE7lpniIqL4fk7MFGBWVZ9b3TB6-X',{
-   host:'kesavan.db.elephantsql.com',
-   dialect: 'postgres',
+const sequelize = new Sequelize('bd_loja','root','',{
+   host:'127.0.0.1',
+   dialect: 'mysql',
    define:{
        charset: 'utf8',
        collate: 'utf8_general_ci',
