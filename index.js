78bc63289be4622f663a7522c9e2d31ea8d7f546
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-09-26 13:40:37 -0400

    Adicionado o modelo Usuario.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.engine('hbs', hbs.engine({
 }));
 app.set('view engine', 'hbs');
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 
 //rota inicial 
---
+++ models/Usuario.js
@@ -0,0 +1,22 @@
+const db = require('./db');
+
+const Usuario = db.sequelize.define('usuario', {
+    id:{
+        type: db.sequeilze.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome:{
+        type: db.sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: db.sequelize.STRING,
+        allowNull: false
+    },
+    senha:{
+        type: db.sequelize.STRING,
+        allowNull: false
+    }
+})
